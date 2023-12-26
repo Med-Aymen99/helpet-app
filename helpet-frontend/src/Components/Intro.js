7b84d4f8d6b7501863e7775ebb631092ed86dc48
jihenedoudech <jihendoudech@insat.u-carthage.tn>
2023-12-26 16:08:47 +0100

    intro modif

--- helpet-frontend/src/Components/Intro.js
+++ helpet-frontend/src/Components/Intro.js
@@ -10,7 +10,7 @@ export default function Intro() {
                 alt=""
             />
             <h1 className="intro--header ">Adopt pets from us</h1>
-            <p className="intro--text">These lovely pets need a home!</p>
+            <p className="intro--text">These adorable pets need a home!</p>
         </section>
     )
 }
